e895d4ef342ff0d8283659d8358406d8f5663ace
Derek Dorr <44913332+dhdorr@users.noreply.github.com>
2023-11-10 14:32:29 -0800

    intro to react project

---
+++ Complete-Intro-To-React/src/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary component caught an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an herror with this listing.{" "}
+          <Link to="/">Click here to go back to the home page.</Link>
+        </h2>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
